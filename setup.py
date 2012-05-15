13bc17c983035a85f974e59638700e84b3335d90
shaung <shaun.geng@gmail.com>
2012-05-16 01:12:43 +0900

    set zip_safe to false for "python -m" using

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = '0.01'
+__VERSION__ = '0.0.1'
 
 params = {
     'name': 'dirtree',
     'version': __VERSION__,
     'description': '',
-    'author': 'shaung<shaun.geng@gmail.com>',
-    'url': '',
+    'author': 'shaung',
+    'author_email': 'shaun.geng@gmail.com',
+    'url': 'https://github.com/shaung/dirtree/',
     'py_modules': ['dirtree'],
     'license': 'BSD',
     'download_url': '',
+    'zip_safe': False,
 }
 
 
