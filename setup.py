5b80cb671973769139538ca1b466f4da538c7c69
shaung <shaun.geng@gmail.com>
2012-05-19 23:36:01 +0900

    setup scripts

--- setup.py
+++ setup.py
@@ -2,10 +2,16 @@
 
 __VERSION__ = '0.0.1'
 
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 params = {
     'name': 'dirtree',
     'version': __VERSION__,
     'description': '',
+    'long_description': read('README.md'),
     'author': 'shaung',
     'author_email': 'shaun.geng@gmail.com',
     'url': 'https://github.com/shaung/dirtree/',
@@ -13,8 +19,15 @@ params = {
     'license': 'BSD',
     'download_url': '',
     'zip_safe': False,
+    'classifiers': [
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    'install_requires': [
+        'argparse',
+    ],
 }
-
 
 from setuptools import setup
 setup(**params)
